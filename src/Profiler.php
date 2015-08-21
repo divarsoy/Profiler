683812684c174408b84733fa49b155575a733869
Dag A. Ivarsøy <divarsoy@siteset.co.uk>
2015-08-21 13:55:34 +0100

    first commit

---
+++ src/Profiler.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Divarsoy;
+
+
+class Profiler
+{
+    protected $startTime;
+    protected $lap;
+
+    public function __construct()
+    {
+        $this->startTime = $this->getTime();
+        $this->lap = $this->startTime;
+    }
+
+    public function getElapsedTime(){
+        $now = $this->getTime();
+        return $now - $this->startTime;
+    }
+
+    public function setLap(){
+        $this->lap = $this->getTime();
+    }
+
+    public function getElapsedLap(){
+        $elapsedTime = $this->getTime() - $this->lap;
+        $this->lap = $this->getTime();
+        return $elapsedTime;
+    }
+
+    protected function getTime()
+    {
+        list($usec, $sec) = explode(" ", microtime());
+        return ((float)$usec + (float)$sec);
+    }
+
+}
